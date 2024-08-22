312b6f2478d4dbc2b2e9bd6c148844679c5c4833
Kyle Kemp <kyle@seiyria.com>
2024-08-22 09:01:37 -0500

    closes #70

--- src/app/shared/components/cell-buttons/cell-buttons.component.ts
+++ src/app/shared/components/cell-buttons/cell-buttons.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
 
@@ -10,6 +11,8 @@ import { ICellRendererParams } from 'ag-grid-community';
 export class CellButtonsComponent implements ICellRendererAngularComp {
   public params!: any;
 
+  public deleteWarnSwal = viewChild<SwalComponent>('deleteItem');
+
   agInit(params: ICellRendererParams) {
     this.params = params;
   }
@@ -17,5 +20,15 @@ export class CellButtonsComponent implements ICellRendererAngularComp {
   refresh(params: ICellRendererParams) {
     this.params = params;
     return true;
+  }
+
+  async attemptDelete($event: any) {
+    const holdingShift = $event.shiftKey;
+    if (!holdingShift) {
+      await this.deleteWarnSwal()?.fire();
+      return;
+    }
+
+    this.params.deleteCallback?.(this.params.data);
   }
 }
