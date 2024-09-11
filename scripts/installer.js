9031851e660c065ce534eab1e0978b7496adc47c
Kyle J. Kemp <kyle@seiyria.com>
2024-09-11 10:32:49 -0500

    test commit

---
+++ scripts/installer.js
@@ -0,0 +1,21 @@
+const electronInstaller = require("electron-winstaller");
+const fs = require("fs");
+
+const generate = async () => {
+  fs.copyFileSync("./LICENSE", "release/win-unpacked/LICENSE");
+
+  try {
+    await electronInstaller.createWindowsInstaller({
+      appDirectory: "./release/win-unpacked",
+      outputDirectory: "./release/win-installer",
+      authors: "Land of the Rair",
+      exe: "Land of the Rair ModKit.exe",
+      version: "1.0.0",
+    });
+    console.log("Success!");
+  } catch (e) {
+    console.log(`No dice: ${e.message}`);
+  }
+};
+
+generate();
