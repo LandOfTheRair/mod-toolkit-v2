58c6cb728aa080eecca6caa715a89ef7bb799cc9
Kyle J. Kemp <kyle@seiyria.com>
2025-10-04 13:54:20 -0500

    add directive

---
+++ src/app/shared/directives/trim.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, effect, model } from '@angular/core';
+
+@Directive({
+  selector: '[autoTrim]',
+})
+export class AutoTrimDirective {
+  public ngModel = model<string>('ngModel');
+
+  private timeoutHandle!: ReturnType<typeof setTimeout>;
+
+  constructor() {
+    effect(
+      () => {
+        clearTimeout(this.timeoutHandle);
+
+        const newValue = this.ngModel();
+
+        this.timeoutHandle = setTimeout(() => {
+          this.ngModel.set(newValue?.replace(/\s+/g, ' ').trim() ?? newValue);
+        }, 500);
+      },
+      { allowSignalWrites: true },
+    );
+  }
+}
